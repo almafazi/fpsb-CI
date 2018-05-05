3632a1969a52167e7b489cb4b17252e0c24b3eb2
mochafaishal <mochammadfaishal21@gmail.com>
2018-05-05 10:47:14 -0700

    pengurus

--- application/views/pengurus.php
+++ application/views/pengurus.php
@@ -2,26 +2,23 @@
 <html lang="en">
 
 <head>
-    <title>Pengurus - Lembaga Eksekutif Mahasiswa Fakultas Psikologi Dan Ilmu Sosial Budaya UII</title>
-    <link rel="shortcut icon" href="<?php echo base_url(); ?>images/logo/favicon.jpg">
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="description" content="Kepengurusan - Situs Resmi Lembaga Eksekutif Mahasiswa Fakultas Psikologi Dan Ilmu Sosial Budaya Universitas Islam Indonesia #thinksmart #workhard." />
-    <meta name="author" content="LEM FPSB UII"/>
-    <meta content="document" name="resource-type"/>
-    <meta content="all" name="audience"/>
-    <meta name="googlebot" content="index,follow"/>
-    <meta name="spiders" content="index,follow" />
-    <meta name="robots" content="index,follow" />
-    <!-- Favicons -->
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/material-kit.css?v=2.0.2">
-    <!-- Documentation extras -->
-    <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="<?php echo base_url(); ?>assets/assets-for-demo/demo.css" rel="stylesheet" />
-    <!-- iframe removal -->
+  <title>Lembaga Eksekutif Mahasiswa Fakultas Psikologi Dan Ilmu Sosial Budaya UII</title>
+  <link rel="shortcut icon" href="images/logo/favicon.jpg">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="author" content="LEM FPSB UII"/>
+  <meta content="document" name="resource-type"/>
+  <meta content="all" name="audience"/>
+  <meta name="googlebot" content="index,follow"/>
+  <meta name="spiders" content="index,follow" />
+  <meta name="robots" content="index,follow" />
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/material-kit.css?v=2.0.2">
+  <link href="<?php echo base_url(); ?>assets/assets-for-demo/demo.css" rel="stylesheet" />
+  <!-- fancybox -->
+  <link href="<?php echo base_url(); ?>assets/css/fancybox.css" rel="stylesheet" />
 </head>
 
 <body class="landing-page ">
@@ -89,62 +86,74 @@
                 </div>
 
                 <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h1 class="mb-4 text-Black text-center">Profil Pengurus</h1>
+                        <div class="row">
+                            <div class="col-md-12 text-center">
+                                <h2 class="title">Our Team</h2>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-2 col-6 p-1" style="margin-left:260px">
+                                <a href="<?php echo base_url(); ?>#">
+                          <a class="single_image" href="<?php echo base_url(); ?>images/ketua.png"><img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/ketua.png"  style="padding:20px"></a>
+                            </div>
+                            <div class="col-md-2 col-6 p-1">
+                                <a href="<?php echo base_url(); ?>#">
+                          <a class="single_image" href="<?php echo base_url(); ?>images/sekre.png"><img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/sekre.png"  style="padding:20px"></a>
+                            </div>
+                            <div class="col-md-2 col-6 p-1">
+                                <a href="<?php echo base_url(); ?>#">
+                                  <a class="single_image" href="<?php echo base_url(); ?>images/bendahara.png"><img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/bendahara.png"  style="padding:20px"></a>
+                            </div>
+
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-2 col-6 p-1" style="margin-left:260px">
-                            <a href="javascript:;">
-                        <img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/ketua.png"  style="padding:20px"> </a>
-                        </div>
-                        <div class="col-md-2 col-6 p-1">
-                            <a href="javascript:;">
-                        <img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/sekre.png"  style="padding:20px"> </a>
-                        </div>
-                        <div class="col-md-2 col-6 p-1">
-                            <a href="javascript:;">
-                        <img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/bendahara.png"  style="padding:20px"> </a>
+                        <div class="row">
+                            <div class="col-md-2 col-6 p-1" style="margin-left:70px">
+                                <a href="<?php echo base_url(); ?>#">
+                                  <a class="single_image" href="<?php echo base_url(); ?>images/jarmedin.png"><img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/jarmedin.png"  style="padding:20px"></a>
+                            </div>
+                            <div class="col-md-2 col-6 p-1">
+                                <a href="<?php echo base_url(); ?>#">
+                                  <a class="single_image" href="<?php echo base_url(); ?>images/po.png"><img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/po.png"  style="padding:20px"></a>
+                            </div>
+                            <div class="col-md-2 col-6 p-1">
+                                <a href="<?php echo base_url(); ?>#">
+                                  <a class="single_image" href="<?php echo base_url(); ?>images/pmb.png"><img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/pmb.png"  style="padding:20px"></a>
+                            </div>
+                            <div class="col-md-2 col-6 p-1">
+                                <a href="<?php echo base_url(); ?>#">
+                                  <a class="single_image" href="<?php echo base_url(); ?>images/sosling.png"><img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/sosling.png"  style="padding:20px"></a>
+                            </div>
+                            <div class="col-md-2 col-6 p-1">
+                                <a href="<?php echo base_url(); ?>#">
+                                  <a class="single_image" href="<?php echo base_url(); ?>images/kd.png"><img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/kd.png"  style="padding:20px"></a>
+                            </div>
+
                         </div>
 
                     </div>
-                    <div class="row">
-                        <div class="col-md-2 col-6 p-1" style="margin-left:70px">
-                            <a href="javascript:;">
-                        <img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/jarmedin.png"  style="padding:20px"> </a>
-                        </div>
-                        <div class="col-md-2 col-6 p-1">
-                            <a href="javascript:;">
-                        <img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/po.png"  style="padding:20px"> </a>
-                        </div>
-                        <div class="col-md-2 col-6 p-1">
-                            <a href="javascript:;">
-                        <img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/pmb.png"  style="padding:20px"> </a>
-                        </div>
-                        <div class="col-md-2 col-6 p-1">
-                            <a href="javascript:;">
-                        <img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/sosling.png"  style="padding:20px"> </a>
-                        </div>
-                        <div class="col-md-2 col-6 p-1">
-                            <a href="javascript:;">
-                        <img class="" width="220px" height="220px" src="<?php echo base_url(); ?>images/kd.png"  style="padding:20px"> </a>
-                        </div>
-
-                    </div>
-                </div>
 
             </div>
         </div>
     </div>
+  </div>
+  
 </body>
 
-
-
-</html>
+<!--   Core JS Files   -->
+<script src="<?php echo base_url(); ?>assets/js/core/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/fancybox.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/core/popper.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootstrap-material-design.js"></script>
+<!--  Plugin for Date Time Picker and Full Calendar Plugin  -->
+<script src="<?php echo base_url(); ?>assets/js/plugins/moment.min.js"></script>
+<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+<script src="<?php echo base_url(); ?>assets/js/plugins/bootstrap-datetimepicker.min.js"></script>
+<!--	Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="<?php echo base_url(); ?>assets/js/plugins/nouislider.min.js"></script>
+<!-- Material Kit Core initialisations of plugins and Bootstrap Material Design Library -->
+<script src="<?php echo base_url(); ?>assets/js/material-kit.js?v=2.0.2"></script>
+<!-- Fixed Sidebar Nav - js With initialisations For Demo Purpose, Don't Include it in your project -->
+<script src="<?php echo base_url(); ?>assets/assets-for-demo/js/material-kit-demo.js"></script>
+<script>
+$(".single_image").fancybox();
+</script>
